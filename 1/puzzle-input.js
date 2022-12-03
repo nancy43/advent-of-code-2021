d3e1a690ab0774f15c981b89dca73b2e7f064617
nancysharma <Amazing@4455>
2022-12-03 17:38:30 -0500

    fix

--- 1/puzzle-input.js
+++ 1/puzzle-input.js
@@ -5,7 +5,8 @@ const puzzleInput = fs
     .readFileSync(path.join(__dirname, 'puzzle-input.txt'), 'utf8')
     .toString()
     .trim()
-
+    .split('\n')
+    .map((num) => parseInt(num, 10));
 module.exports = {
     puzzleInput,
 };
