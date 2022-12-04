99ad57c6967f03da53724b2c196a852beaad8da4
nancysharma <Amazing@4455>
2022-12-03 20:48:02 -0500

    update

--- 1/part-two.js
+++ 1/part-two.js
@@ -2,9 +2,9 @@ const { puzzleInput } = require('./puzzle-input');
 function slidingWindow (){
     let count = 0;
     for (let i = 0; i < puzzleInput.length-3; i++) {
-       let sum0 = (puzzleInput[i]+puzzleInput[i+1]+puzzleInput[i+2]);
-       let sum1 = (puzzleInput[i+1]+puzzleInput[i+2]+puzzleInput[i+3]);
-       if (sum1 > sum0){
+       let current = (puzzleInput[i]+puzzleInput[i+1]+puzzleInput[i+2]);
+       let next = (puzzleInput[i+1]+puzzleInput[i+2]+puzzleInput[i+3]);
+       if (next > current){
            count ++;
        }
     }
