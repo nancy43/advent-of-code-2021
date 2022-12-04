ac56a2073d986fb76cbdb6f12727d4b58825a985
nancysharma <Amazing@4455>
2022-12-04 00:30:11 -0500

    Logic for Part 2

--- 2/part-two.js
+++ 2/part-two.js
@@ -1,14 +1,19 @@
 const { input } = require('./input');
 function submarine () {
-    let finalHorizon = 0;
-    let finalDepth = 0;
-    let aim = 0;
-    for (let {direction, value} of input) {
+    let [aim, finalHorizontal, finalDepth ] = [0,0,0];
+    for (let { direction, value } of input) {
         if (direction === 'forward') {
-            finalHorizon += value;
+            finalHorizontal += value;
             finalDepth += aim * value;
         }
-        console.log(finalHorizon)
+        else if (direction === 'up') {
+            aim -= value;
+        } else if (direction === 'down') {
+            aim += value;
+        } else {
+            return false;
+        }
     }
+    console.log(finalDepth * finalHorizontal);
 }
 submarine()
