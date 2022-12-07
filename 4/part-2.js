a7d8e95a6bbf83a99d4635e184dba49179ba98f1
nancysharma <Amazing@4455>
2022-12-07 14:05:14 -0500

    Logic for the final score of last winning board

---
+++ 4/part-2.js
@@ -0,0 +1,4 @@
+import {finalScoresList} from './part-1.js';
+//The last winning score from the finalScoresList would be the score of last winning board
+const LastBoardScore = finalScoresList[finalScoresList.length - 1];
+console.log("Final score for the last winning board is:",LastBoardScore)
